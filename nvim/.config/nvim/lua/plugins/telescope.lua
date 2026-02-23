c466b39f350ff3410fb8b49d81d48b7055f5d830
Thien Truong <truongcongthien.dev@gmail.com>
2026-02-23 11:39:23 +0700

    update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
 		local actions = require("telescope.actions")
 		local keymap = vim.keymap
 
+		-- Keymaps
 		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 		keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
@@ -18,6 +19,7 @@ return {
 						["<esc>"] = actions.close,
 					},
 				},
+
 				file_ignore_patterns = {
 					"node_modules",
 					".git",
@@ -26,6 +28,7 @@ return {
 					"%.lock",
 					"codegen.ts",
 				},
+
 				dynamic_preview_title = true,
 				path_display = { "smart" },
 				layout_strategy = "horizontal",
@@ -36,9 +39,22 @@ return {
 					},
 				},
 			},
+
 			pickers = {
 				find_files = {
 					hidden = true,
+					initial_mode = "normal",
+				},
+
+				buffers = {
+					initial_mode = "normal",
+					sort_mru = true,
+					ignore_current_buffer = true,
+					show_all_buffers = false,
+				},
+
+				live_grep = {
+					initial_mode = "insert",
 				},
 			},
 		})
